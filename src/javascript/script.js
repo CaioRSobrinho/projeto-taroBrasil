b606d1e9eb8da459b9de855dddf38e795bcf7c37
Caio Ruan <caiosobrinhosilva@gmail.com>
2025-02-03 17:49:28 -0300

    Update animação em javascript  do banner e tarja

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,3 +5,32 @@ $(document).ready(function(){
     })
 
 })
+
+$(window).on('scroll', function() {
+    const header = $('header')
+    const scrollPosition = $(window).scrollTop() - header.outerHeight()
+    let activeSectionIndex = 0
+
+    if(scrollPosition <= 0) {
+        header.css('box-shadow', 'none')
+    } else {
+        header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)')
+    }
+})
+
+ScrollReveal().reveal('#cta', {
+    origin: 'left',
+    duration: 2000,
+    distance:'20%'
+})
+
+ScrollReveal().reveal('#banner', {
+    origin: 'right',
+    duration: 2000,
+    distance:'20%'
+})
+ScrollReveal().reveal('#list_tarja', {
+    origin: 'top',
+    duration: 1000,
+    distance:'20%'
+})
